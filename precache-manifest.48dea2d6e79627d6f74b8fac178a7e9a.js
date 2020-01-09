889318add02e05d12353bd32b178360b0119aa25
iFsi DeCoDe <ifsidecode@gmail.com>
2020-01-09 15:11:05 +0100

    prototype

---
+++ precache-manifest.48dea2d6e79627d6f74b8fac178a7e9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ff005ed374bf4c857cae133b24d4a39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c31553a9cc5ab6aef719",
+    "url": "/static/css/main.de3d569a.chunk.css"
+  },
+  {
+    "revision": "49e6c6748fac6e5d494a",
+    "url": "/static/js/2.633546cb.chunk.js"
+  },
+  {
+    "revision": "cbac7ff58cded3b89c2259c2649eb399",
+    "url": "/static/js/2.633546cb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c31553a9cc5ab6aef719",
+    "url": "/static/js/main.112f103c.chunk.js"
+  },
+  {
+    "revision": "aa9cd3caf36c26d4f89c",
+    "url": "/static/js/runtime-main.7a9ef57c.js"
+  }
+]);
